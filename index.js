4567721d699a1b732e39b907ee313564210d0c14
Tayyaba Naseer Ahmed <tayyabanaseerahmed6@gmail.com>
2024-03-19 00:10:11 +0500

    simple_Calculator

---
+++ index.js
@@ -0,0 +1,24 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{ message: "Enter first number: ", type: "number", name: "FirstNumber" },
+    { message: "Enter second number: ", type: "number", name: "SecondNumber" },
+    { message: "select one of the operator to perform operator: ",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    }
+]);
+if (answer.operator === "Addition") {
+    console.log(answer.FirstNumber + answer.SecondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.FirstNumber - answer.SecondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.FirstNumber * answer.SecondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.FirstNumber / answer.SecondNumber);
+}
+else {
+    console.log("Please select valid operator");
+}
